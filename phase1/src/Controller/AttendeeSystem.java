363470ceb6bef4e6205f720e79e003995ac021e8
Kaiwen Chen <kaiwenn.chen@mail.utoronto.ca>
2020-11-15 03:56:03 +0800

    Restructured AttendeeSystem; Message part not yet implemented

--- phase1/src/Controller/AttendeeSystem.java
+++ phase1/src/Controller/AttendeeSystem.java
@@ -1,76 +1,103 @@
 package Controller;
-import Entity.*;
 import UI.AttendeeUI;
 import UseCase.LoginManager;
 import UseCase.StrategyManager;
 import UseCase.TalkManager;
 import UseCase.MessageManager;
+import UseCase.AttendeeManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
-//Completeness:25%
 
 public class AttendeeSystem {
     protected LoginManager loginM;
     protected TalkManager talkManager;
-    protected Attendee currAttendee;
     protected MessageManager MsgM;
     protected AttendeeUI attendeeUI;
     protected StrategyManager strategyM;
+    protected AttendeeManager attendeeM;
 
-    public AttendeeSystem(LoginManager loginM){
+    public AttendeeSystem(LoginManager loginM, TalkManager TalkM, MessageManager MsgM, AttendeeUI attendeeUI,
+                         StrategyManager StrategyManager, AttendeeManager AttendeeM) {
         this.loginM = loginM;
+        this.talkManager = TalkM;
+        this.MsgM = MsgM;
+        this.attendeeUI = attendeeUI;
+        this.strategyM = StrategyManager;
+        this.attendeeM = AttendeeM;
+
     }
 
     public void run() {
-        int userInput = -1;
-        while (userInput != 5) {
+        int userChoice;
+        do {
             attendeeUI.startup();
+            userChoice = chooseMode1();
+            enterBranch(userChoice);
+        } while (userChoice != 5);
+    }
 
-            userInput = chooseMode1();
-
-            switch (userInput) {
-                case 1:
-                    //readMyTalks();
-                    break;
-                case 2:
-                    attendeeUI.signUpTalk();
-                    //readAllTalks(); *only show available talk
-                    //scan the input *if valid
-                    //add talk into the schedule
-                    //prompt: success
-
-                    break;
-                case 3:
-                    attendeeUI.cancelTalk();
-                    //readMyTalks();
-                    //scan the input
-                    //remove the talk from the schedule
-                    //prompt: success
-
-                    break;
-                case 4:
-                    //message part
-                    attendeeUI.msgSelect();
-                    //will have another 'switch cases'
-                default:
-                    break;
-
-            }
-            System.out.println("Quit Messaging System");
+    private void enterBranch(int userChoice){
+        switch (userChoice){
+            case 1:
+                readAllMyTalks();
+                break;
+            case 2:
+                signUpMyNewTalks();
+            case 3:
+                cancelMyTalks();
+            case 4:
+                MsgDashboard();
+            case 5:
+                break;
         }
     }
 
-    private void readMyTalks() {
+    private void MsgDashboard(){
+        int userChoice;
+        do{
+            attendeeUI.msgSelect();
+            userChoice = chooseMode2();
+            msgOp(userChoice);
+        } while (userChoice != 4);
+
+
     }
 
-    private void readAllTalks() {
+    private void msgOp(int userChoice){
+        switch (userChoice){
+            case 1:
+                //msgToAttendee();
+                break;
+            case 2:
+                //msgToSpeaker();
+                break;
+            case 3:
+                //inbox();//check received message
+                break;
+            case 4:
+                break;
+        }
     }
 
+    private int chooseMode1(){    //For Attendee Dashboard.
+        ArrayList<Integer> validChoices = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        String userInput;
+        int mode = -1;
+        boolean valid = false;
+        while(!valid){
+            userInput = attendeeUI.getrequest();
+            if (!strategyM.isValidChoice(userInput, validChoices))
+                attendeeUI.informinvalidchoice();
+            else {
+                valid = true;
+                mode = Integer.parseInt(userInput);}
+        }
+        return mode;
+    }
 
-    private int chooseMode1(){
+    private int chooseMode2(){    //For MsgDashboard.
         ArrayList<Integer> validChoices = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         String userInput;
         int mode = -1;
@@ -85,4 +112,68 @@ public class AttendeeSystem {
         }
         return mode;
     }
+
+    private void readAllMyTalks(){
+        String a = "My signed up talks";
+        ArrayList<Integer> alltalks = attendeeM.getAllMyTalks(); //manager need to implement
+        for(int i = 0; i < alltalks.size(); i++){a += talkManager.gettalkinfo(alltalks.get(i));};
+        attendeeUI.show(a);
+    }
+
+    private void readAllAvailableTalks(){
+        String a = "Available Talks";
+        ArrayList<Integer> alltalks = attendeeM.getAllAvailableTalks(); //manager need to implement
+        for(int i = 0; i < alltalks.size(); i++){a += talkManager.gettalkinfo(alltalks.get(i));};
+        attendeeUI.show(a);
+    }
+
+    private int targetTalksSignUp(){
+        ArrayList<Integer> validChoices = attendeeM.getAllAvailableTalks();//manager need to implement
+        String userInput;
+        int mode = -1;
+        boolean valid = false;
+        while(!valid){
+            userInput = attendeeUI.getrequest2();
+            if (!strategyM.isValidChoice(userInput, validChoices))
+                attendeeUI.informinvalidchoice();
+            else {
+                valid = true;
+                mode = Integer.parseInt(userInput);}
+        }
+        return mode;}
+
+    private void signUpMyNewTalks(){
+        attendeeUI.signUpTalk();
+        readAllAvailableTalks();
+        int input = targetTalksSignUp();
+        attendeeM.enrol(input);
+        attendeeUI.signUpSuc();
+    }
+
+    private int targetTalksCancel(){
+        ArrayList<Integer> validChoices = attendeeM.getAllMyTalks();//manager need to implement
+        String userInput;
+        int mode = -1;
+        boolean valid = false;
+        while(!valid){
+            userInput = attendeeUI.getrequest3();
+            if (!strategyM.isValidChoice(userInput, validChoices))
+                attendeeUI.informinvalidchoice();
+            else {
+                valid = true;
+                mode = Integer.parseInt(userInput);}
+        }
+        return mode;}
+
+    private void cancelMyTalks(){
+        attendeeUI.cancelTalk();
+        readAllMyTalks();
+        int input = targetTalksCancel();
+        attendeeM.drop(input);//manager need to implement
+        attendeeUI.cancelSuc();
+    }
+
 }
+
+
+
--- phase1/src/UI/AttendeeUI.java
+++ phase1/src/UI/AttendeeUI.java
@@ -13,10 +13,20 @@ public class AttendeeUI {
     //not yet implement the message part
     public void startup() {
         System.out.println("----------------AttendeeSystem-----------------\nHi, " +
-                "Attendee! Would you like to\n1 -> See your talk schedule\n2 -> Sign up a new talk\n3 -> Cancel a talk \n4 -> Quit");
+                "Attendee! Would you like to\n1 -> View your signed up talks\n2 -> Sign up for a new talk\n3 -> Cancel a talk\n4 ->Send message \n5 -> Quit");
     }
     public String getrequest(){
         System.out.println("Please Enter Your Response");
+        return attendeescanner.nextLine();
+    }
+
+    public String getrequest2(){
+        System.out.println("Please Enter The Valid ID of the Talk. Enter ");
+        return attendeescanner.nextLine();
+    }
+
+    public String getrequest3(){
+        System.out.println("Please Enter The Valid ID of one of your talk. Enter ");
         return attendeescanner.nextLine();
     }
 
@@ -29,14 +39,28 @@ public class AttendeeUI {
         System.out.println("----------------Signing up a talk-----------------\nPlease enter the id of the talk that you want to sign up");
     }
 
+    public void signUpSuc(){
+        System.out.println("You have signed up a new talk.");
+    }
+
     public void cancelTalk(){
         System.out.println("----------------Cancelling a talk-----------------\nPlease enter the id of the talk that you want to cancel");
     }
 
+    public void cancelSuc(){
+        System.out.println("You have canceled one of your talks.");
+    }
+
     public void msgSelect(){
         System.out.println("----------------Message Interface-----------------\nHi, " +
-                "Would you like to\n1 -> View your inbox\n2 -> Send a message");
+                "Would you like to\n1 -> Send Message to a attendee\n2 -> Send a message to the speaker of a talk\n3 -> View your inbox\n4 -> Quit");
     }
+
+    public void show(String a){System.out.println(a);}
 }
 
-
+    /*
+    public void signUpTalk(){
+        System.out.println("");
+    }
+    */
