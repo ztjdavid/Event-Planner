52be287f96a1f75aff57b1263822a9ead9ff4ee9
Yixin Chen <yixinlouisa.chen@mail.utoronto.ca>
2020-11-16 03:48:44 -0500

    Implemented javadoc for entity Message

--- phase1/src/Entity/Message.java
+++ phase1/src/Entity/Message.java
@@ -20,14 +20,42 @@ public class Message {
         this.replies = new ArrayList<>();
     }
 
+    /**
+     * Get the Message ID.
+     * @return the Message's ID.
+     */
      public int getmessageid(){
         return this.messageid;
      }
 
+    /**
+     * Get the sender's ID.
+     * @return the Message's sender's ID.
+     */
      public int getSenderid(){return this.senderid;}
+
+    /**
+     * Get the getter's ID.
+     * @return the Message's getter's ID.
+     */
      public int getGetterid(){return this.getterid;}
+
+    /**
+     * Get the Message's text.
+     * @return the string representation of the Message's text.
+     */
      public String getTxt(){return this.txt;}
+
+    /**
+     * Add the given reply message ID to the Message's arraylist of reply IDs.
+     * @param messageid the ID of the Message.
+     */
      public void addReply(int messageid){this.replies.add(messageid);}
+
+    /**
+     * Get all the reply ID of the Message as an arraylist.
+     * @return all the replies of the Message.
+     */
      public ArrayList<Integer> getAllReplies(){return new ArrayList<>(this.replies);}
 
 
