49105afc618260593da02bf160ef6ef4f3198e95
Gareth Chen <garethcxy.chen@mail.utoronto.ca>
2020-11-27 10:21:48 -0500

    add javadoc in Presenter.java

--- phase2/src/Presenters/Presenter.java
+++ phase2/src/Presenters/Presenter.java
@@ -2,6 +2,9 @@ package Presenters;
 
 import java.util.ArrayList;
 
+/**
+ * A general presenter class with basic methods. All other presenter classes extend it.
+ */
 public class Presenter {
     public ITextUI textUI;
 
@@ -9,20 +12,37 @@ public class Presenter {
         this.textUI = UI;
     }
 
-
+    /**
+     * Print text on screen(Console).
+     * @param txt String being printed.
+     */
     public void printText(String txt) {
         textUI.print(txt);
     }
 
+    /**
+     * Ask user for input.
+     * @return User's input in next line.
+     */
     public String requestInput(){
         return textUI.request();
     }
 
+    /**[Overload]
+     * Ask user for input.
+     * @param requestInfo Message printed before user giving input.
+     * @return User's input in next line.
+     */
     public String requestInput(String requestInfo){
         printText(requestInfo);
         return requestInput();
     }
 
+    /**
+     * Ask user for a numeric input. If not, keep requesting.
+     * @param requestInfo Message printed before user giving input.
+     * @return Integer which matches user's input.
+     */
     public int requestNumInput(String requestInfo){
         int num = -2;
         boolean exit;
@@ -38,8 +58,18 @@ public class Presenter {
         return num;
     }
 
+    /**
+     * Ask user to press Enter.
+     */
     public void pause(){ requestInput(); }
 
+    /**
+     * Ask user to choose an option(numeric).
+     * @param choiceList Valid options.
+     * @param requestInfo Message printed before user giving input.
+     * @param invalidInfo Message printed if user gives an invalid choice.
+     * @return User's choice.
+     */
     public int chooseOption(ArrayList<Integer> choiceList, String requestInfo,
                             String invalidInfo){
         boolean exit = false;
@@ -53,6 +83,14 @@ public class Presenter {
         return Integer.parseInt(userInput);
     }
 
+    /**[Overload]
+     * Ask user to choose an option(numeric).
+     * @param choiceList Valid options.
+     * @param requestInfo Message printed before user giving input.
+     * @param invalidInfo Message printed if user gives an invalid choice.
+     * @param successInfo Message printed if user gives an valid choice.
+     * @return User's choice.
+     */
     public int chooseOption(ArrayList<Integer> choiceList, String requestInfo,
                             String invalidInfo, String successInfo){
         boolean exit = false;
