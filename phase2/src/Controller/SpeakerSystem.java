bfec61d5abdc4eb64ec50cd974308ef18be5b442
Yixin Chen <yixinlouisa.chen@mail.utoronto.ca>
2020-12-02 17:37:00 -0500

    Added get all unread messages and get all read messages of a speaker functionalities of SpeakerSystem

--- phase2/src/Controller/SpeakerSystem.java
+++ phase2/src/Controller/SpeakerSystem.java
@@ -1,4 +1,6 @@
 package Controller;
+import Entity.Message;
+import Entity.Speaker;
 import Presenters.SpeakerUI;
 import UseCase.*;
 
@@ -339,5 +341,32 @@ public class SpeakerSystem {
         return eventManager.getallattendee(talklist);
     }
 
+    ///// Louisa added
+    public ArrayList<Message> getAllUnread(int speakerId){
+        Speaker acc = (Speaker) SpeakerM.getAccountWithId(speakerId);
+        ArrayList<Integer> inbox = acc.getInbox();
+        ArrayList<Message> unread = new ArrayList<>();
+        for(Integer i : inbox){
+            Message msg = MsgM.getmessage(i);
+            if(!msg.getReadStatus()){
+                unread.add(msg);
+            }
+        }
+        return unread;
+    }
+    public ArrayList<Message> getAllRead(int speakerId){
+        Speaker acc = (Speaker) SpeakerM.getAccountWithId(speakerId);
+        ArrayList<Integer> inbox = acc.getInbox();
+        ArrayList<Message> read = new ArrayList<>();
+        for(Integer i : inbox){
+            Message msg = MsgM.getmessage(i);
+            if(!msg.getReadStatus()){
+                read.add(msg);
+            }
+        }
+        return read;
+    }
+
+    /////
 
 }
